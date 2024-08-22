491f921c0259aee723e0d5c0baba67986d5b7d6e
wangzicong <maclaurin821@gmail.com>
2024-08-22 17:59:35 +0800

    0822更新

--- main.py
+++ main.py
@@ -6,15 +6,21 @@ import logging
 from tqdm import tqdm
 import uuid
 import os
-from transformers import BertTokenizer, BertForSequenceClassification
+from transformers import BertTokenizer, BertForSequenceClassification, AutoTokenizer, AutoModelForSequenceClassification
 import torch
 import re
 import torch.nn.functional as F
-# 加载分词器和模型
-tokenizer = BertTokenizer.from_pretrained('bert-base-chinese')
-ranker_model = BertForSequenceClassification.from_pretrained('./ranker_model/')
-# 加载分词器和模型
-model_text_by = BertForSequenceClassification.from_pretrained('./model_text_by/')
+import copy
+
+def get_label(premise, hypothesis):
+    input = tokenizer_relation(premise, hypothesis, truncation=True, return_tensors="pt").to(device)
+    output = model_relation(input["input_ids"].to(device))
+    prediction = torch.softmax(output["logits"][0], -1).tolist()
+    label_names = ["包含", "中性", "矛盾"]
+    prediction = {name: round(float(pred) * 100, 1) for pred, name in zip(prediction, label_names)}
+    max_label = max(prediction, key=prediction.get)
+    return max_label
+
 
 
 # 获取prompt
@@ -44,13 +50,13 @@ def _internal_model_generate(model, t_trace_id, p_score_prompt: str):
     data = Conversation(prompt_data, gpt_internal_api=True)
     return model.generate_single(data)
 # 获取QWEN模型的回答
-def _internal_model_generate_QWEN(model, t_trace_id: str, p_score_prompt: str):
+def _internal_model_generate_QWEN(model, t_trace_id: str, p_score_prompt: list):
     if not t_trace_id:
         t_trace_id = str(uuid.uuid4())
     prompt_data = {
         "trace_id": t_trace_id,
         "system_content":'',
-        "messages": [{"role": "user", "content" : p_score_prompt}],
+        "messages": p_score_prompt,
         "top_p": 0.85,
         "top_k": 1,
         "temperature": 1,
@@ -76,7 +82,7 @@ def question_2_complication(text, history):
     PROMPT_Increased = get_prompt_data(r'D:\training_complicating_data\PROMPT', 'Increased_Reasoning_Steps_2')
     PROMPT = random.choice([PROMPT_Deepening, PROMPT_Concretizing, PROMPT_Increased])
     prompt = PROMPT.format(origin_instruction=text, history=history)
-    generated_data = _internal_model_generate_QWEN(model_Qwen, '1', prompt)
+    generated_data = _internal_model_generate_QWEN(model_Qwen, '1', [{"role": "user", "content" : prompt}])
     return generated_data
 # 生成候选高级指令合集
 def advance_inst_generate(inst, history):
@@ -138,19 +144,56 @@ def process_question(topic):
     prompt = PROMPT.format(topic=topic)
     generated_data = _internal_model_generate(model_gpt, "text", prompt)
     return [generated_data]
+def inst_merge(inst):
+    for i in range(len(inst)):
+        for j in range(i + 1, len(inst)):
+            if get_label(inst[i], inst[j]) == "矛盾":
+                return False
+    return True
+
+
+
+def inst_structure(text, data, func):
+    text_now = copy.deepcopy(text)
+    text_now[-1]['content'] = text_now[-1]['content'] + "\n 回答要求如下"
+    for i, item in enumerate(data):
+        text_now[-1]['content'] = f"{text_now[-1]['content']}\n{i + 1}. {item}"
+    res = _internal_model_generate_QWEN(model_Qwen, "text", text_now)
+    func_str = func
+    for item in func_str:
+        exec(item, globals())
+        # 现在 evaluate 函数已经在当前命名空间定义好了
+        # 你可以直接调用它并传入 res 作为参数
+        result = evaluate(res)
+        if result == False:
+            return False
+    return res, text_now
 
 
 if __name__ == '__main__':
-    model_url_qwen = ''
-    user_token_qwen = ''
-    model_url_gpt = ''
-    user_token_gpt = ''
+    # 加载分词器和模型
+    tokenizer = BertTokenizer.from_pretrained('bert-base-chinese')
+    ranker_model = BertForSequenceClassification.from_pretrained('./ranker_model/')
+    # 加载分词器和模型
+    model_text_by = BertForSequenceClassification.from_pretrained('./model_text_by/')
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    tokenizer_relation = AutoTokenizer.from_pretrained("MoritzLaurer/mDeBERTa-v3-base-xnli-multilingual-nli-2mil7")
+    model_relation = AutoModelForSequenceClassification.from_pretrained(
+        "MoritzLaurer/mDeBERTa-v3-base-xnli-multilingual-nli-2mil7").to(device)
+    model_url_qwen = 'http://1411998742277274.cn-wulanchabu.pai-eas.aliyuncs.com/api/predict/rlhf_qwen_2_72b/v1/chat/completions'
+    user_token_qwen = 'MGIwNTU3NmNiZTZmMjE2MWJmYTFhOTcwNWYxZGRiYTE4NzJmMmI4NQ=='
+    model_url_gpt = 'http://61.129.116.136:8651/chatCompletion'
+    user_token_gpt = '240151_aN8z2gtiudKoinIfvcwd'
     model_Qwen = QwenApi(model_url_qwen, token=user_token_qwen)
     model_gpt = GptIntApi(model_url_gpt, token=user_token_gpt)
+    with open(r'D:\training_complicating_data\data\cross_validation_4_2.jsonl', 'r', encoding='utf-8') as f:
+        data_inst_structure = [json.loads(line) for line in f]
+
+
     inst = ['获取详细信息并确认', '请求详细解释和定义', '引入新元素并提出问题', '请求具体技术示例', '内容生成与优化', '聚焦核心要素与情感', '详细操作信息交流', '确保内容完整规范', '反馈与正向激励', '获取项目资金需求', '探索艺术的多样表现', '引导信息扩展与推进', '深入探讨与应用', '利用外部资源支持', '寻求优化和解决方案', '寻求详细信息和反馈', '逐层深入探讨', '优化和扩展内容', '明确需求并设定条件', '优化沟通与深入探讨', '澄清需求并寻求具体指导', '拓展与创新思维', '通过示例和反馈提升理解', '分解复杂任务', '突出机会并请求细节', '提炼并保存最终成果', '制定计划并寻求反馈', '激发对能力的思考', '集中资源达成目标', '获取详细信息', '挑战权威以获取真相', '创造性命名策略', '请求恢复先前状态', '问题解决导向对话', '情境分析与需求满足', '逐步深入探讨相关主题', '请求详细信息和背景', '明确需求并寻求详细说明', '引导轻松对话', '探索精神修行路径', '寻求全面和深入的见解', '提升交流与思维', '获取隐私相关信息', '提出新问题和解决方案', '获取详细信息和指导', '寻求解决方案与优化', '寻求学术与学习资源', '澄清需求并提供详细说明', '使用具体示例增强理解', '内容生成与优化', '探索科技公司动态', '扩展知识与视野', '生成主题相关内容', '生成营销推广内容', '寻求紧急健康援助', '验证信息的准确性', '评估方案的潜在缺陷', '促进合作与项目推广', '寻求详细信息和深入分析', '定制化解决方案与优化', '扩展与澄清信息', '明确需求并逐步指导', '资源优化与推广', '制定个性化解决方案', '制定系统化学习计划', '确保对话连贯并详细扩展', '引入新元素并扩展', '请求具体示例和实现方法', '明确需求并提出疑问', '分解任务并提供具体指导', '简化信息呈现', '请求详细解释与示例', '生成创意社交媒体内容', '优先考虑安全与舒适', '阐述项目及其意义', '寻求信息和建议', '调试和验证查询错误', '深入挖掘和扩展信息', '针对特定需求提供解决方案', '寻求信息并澄清概念', '系统化分析与具体执行', '探讨社会责任与影响', '生成优化内容建议', '营造轻松互动', '综合策略与防御', '促进合作与发展', '比较与优化策略', '寻求具体解决方案和实现方法', '请求和提供具体示例', '确保地址栏一致性', '比较区域社会文化', '分析与讨论政策', '细化内容结构', '探索新领域', '根据内容创建表格', '切换话题或回到之前话题', '重复提问以求新的回答', '代词、指示词指代或省略指代词', '生成表格数据']
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     logger = logging.getLogger()
-    file_handler = logging.FileHandler('log', encoding='utf-8')
+    file_handler = logging.FileHandler('output/log', encoding='utf-8')
     file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
     logger.addHandler(file_handler)
     with open(r'D:\training_complicating_data\data\question.txt', 'r', encoding='utf-8') as f:
@@ -166,28 +209,46 @@ if __name__ == '__main__':
         res = []
         # 生成第一个问题
         instruction = process_question(topic)
-        number = random.randint(2, 5)
+        number = random.randint(3, 7)
         logging.info(f"第一个问题迭代次数: {number}")
         for j in tqdm(range(number)):
             k = question_1_complication(instruction[-1])
             instruction.append(k)
-            if not question_is_equal(instruction[-1], instruction[-2]):
-                break
         res.append({"role": "user", "content": instruction[-1]})
         # print(res)
         # 开始轮次循环
         T = 0
-        round = random.randint(2, 5)
-        logging.info(f"轮次数量为: {round}")
-        while T < round:
+        round_2 = random.randint(3, 4)
+        logging.info(f"轮次数量为: {round_2}")
+        while T < round_2:
+            # 生成上一个问题的输出格式指令以及回答
+            inst_number = random.randint(2, 4)
+            inst_sample = random.sample(data_inst_structure, inst_number)
+            inst_room = [item['instruction'] for item in inst_sample]
+            func = [item['eval_func'][0][0] for item in inst_sample]
+            m = inst_structure(res, inst_room, func)
+            t = 0
+            while not inst_merge(inst_room) or not m:
+                logging.info(f"输出指令在第{t}次执行时未确定")
+                inst_number = random.randint(2, 4)
+                inst_sample = random.sample(data_inst_structure, inst_number)
+                inst_room = [item['instruction'] for item in inst_sample]
+                func = [item['eval_func'][0][0] for item in inst_sample]
+                m = inst_structure(res, inst_room, func)
+                t += 1
+            answer = m[0]
+            res = m[1]
+            logging.info(f"输出指令在第{t}次执行时已确定，指令为{inst_room}，answer前10个字符为{answer[:10]}")
+            # logging.info(f"此轮指令未生成，使用原始指令。")
+            # answer = _internal_model_generate_QWEN(model_Qwen, "text", res)
             # 生成回答
-            answer = _internal_model_generate_QWEN(model_Qwen, "text", str(res))
             # 判断回答是否合理
             #
             #
             #
             # 还没做，烦死了这玩意儿
             res.append({"role": "assistant", "content": answer})
+            logging.info(f"当前回答为: {res}")
             # 生成高级指令
             advance_inst = advance_inst_generate(inst, answer)
             # 生成下一个问题
@@ -205,13 +266,31 @@ if __name__ == '__main__':
                 I += 1
             res.append({"role": "user", "content": question[-1]})
             T += 1
-        answer = _internal_model_generate_QWEN(model_Qwen, "text", str(res))
+
+        # 生成上一个问题的输出格式指令以及回答
+        inst_number = random.randint(2, 4)
+        inst_sample = random.sample(data_inst_structure, inst_number)
+        inst_room = [item['instruction'] for item in inst_sample]
+        func = [item['eval_func'][0][0] for item in inst_sample]
+        m = inst_structure(res, inst_room, func)
+        t = 0
+        while not inst_merge(inst_room) or not m:
+            logging.info(f"输出指令在第{t}次执行时未确定")
+            inst_number = random.randint(2, 4)
+            inst_sample = random.sample(data_inst_structure, inst_number)
+            inst_room = [item['instruction'] for item in inst_sample]
+            func = [item['eval_func'][0][0] for item in inst_sample]
+            m = inst_structure(res, inst_room, func)
+            t += 1
+        answer = m[0]
+        res = m[1]
+        logging.info(f"输出指令在第{t}次执行时已确定，指令为{inst_room}，answer前10个字符为{answer[:10]}")
         # 判断回答是否合理
         #
         #
         # 还没做，烦死了这玩意儿
         res.append({"role": "assistant", "content": answer})
         logging.info(f"最后生成的回答为: {answer}")
-        with open(f'./output/question{i + 7}.json', 'w', encoding='utf-8') as f:
+        with open(f'./output/question{i + 400}.json', 'w', encoding='utf-8') as f:
             json.dump(res, f, ensure_ascii=False, indent=4)
-        logging.info(f"结果已写入文件: ./output/question{i + 7}.json")
+        logging.info(f"结果已写入文件: ./output/question{i + 400}.json")
